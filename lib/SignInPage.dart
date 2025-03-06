f8e58ab4cafa044eb7ac9a1961f9df8adc0c0281
madhavkr1506 <madhavkr9153276724@gmail.com>
2025-03-06 11:15:11 +0530

    Code Updated

--- lib/SignInPage.dart
+++ lib/SignInPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recipe/MenuPage.dart';
+import 'package:recipe/data/local/DBHelper.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -11,6 +12,19 @@ class SignIn extends StatefulWidget {
 class SignInUtil extends State<SignIn> {
   bool Visible = true;
   String buttonText = "";
+  bool ignore = true;
+  var name = TextEditingController();
+  var email = TextEditingController();
+  var contact = TextEditingController();
+  var address = TextEditingController();
+
+  DBHelper? db;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    db = DBHelper.getInstance;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +62,8 @@ class SignInUtil extends State<SignIn> {
                             center: Alignment.center)),
                     child: Text(
                       "One cannot think well, love well, sleep well, if one has not dined well\n- Virginia Woolf",
-                      style: TextStyle(fontStyle: FontStyle.normal, fontSize: 20),
+                      style:
+                          TextStyle(fontStyle: FontStyle.normal, fontSize: 20, color: Colors.pink),
                       textAlign: TextAlign.center,
                     ),
                   ),
@@ -71,41 +86,78 @@ class SignInUtil extends State<SignIn> {
                         Padding(
                           padding: EdgeInsets.all(15),
                           child: TextField(
+                            controller: name,
                             decoration: InputDecoration(
-                              labelText: "Name",
-                              icon: Icon(
-                                Icons.verified_user,
-                                color: Colors.green,
-                              ),
-                              border: OutlineInputBorder(),
-                            ),
+                                labelText: "Name",
+                                hintText: "Input Name",
+                                hintStyle: TextStyle(color: Colors.pinkAccent, fontStyle: FontStyle.normal),
+                                labelStyle: TextStyle(color: Colors.pink),
+                                icon: Icon(
+                                  Icons.verified_user,
+                                  color: Colors.green,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  borderSide:
+                                      BorderSide(color: Colors.pink, width: 2),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  borderSide:
+                                      BorderSide(color: Colors.pink, width: 2),
+                                )),
                           ),
                         ),
                         Padding(
                           padding: EdgeInsets.all(15),
                           child: TextField(
+                            controller: email,
                             decoration: InputDecoration(
-                              labelText: "Email",
-                              icon: Icon(
-                                Icons.email,
-                                color: Colors.red,
-                              ),
-                              border: OutlineInputBorder(),
-                            ),
+                                labelText: "Email",
+                                hintText: "Input Email",
+                                hintStyle: TextStyle(color: Colors.pinkAccent, fontStyle: FontStyle.normal),
+                                labelStyle: TextStyle(color: Colors.pink),
+                                icon: Icon(
+                                  Icons.email,
+                                  color: Colors.red,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  borderSide:
+                                      BorderSide(color: Colors.pink, width: 2),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  borderSide:
+                                      BorderSide(color: Colors.pink, width: 2),
+                                )),
                           ),
                         ),
                         Visibility(
                           child: Padding(
                             padding: EdgeInsets.all(15),
                             child: TextField(
+                              controller: contact,
                               decoration: InputDecoration(
-                                labelText: "Contact Number",
-                                icon: Icon(
-                                  Icons.mobile_friendly,
-                                  color: Colors.yellow,
-                                ),
-                                border: OutlineInputBorder(),
-                              ),
+                                  labelText: "Contact Number",
+                                  hintText: "Input Mobile",
+                                  hintStyle: TextStyle(color: Colors.pinkAccent, fontStyle: FontStyle.normal),
+                                  prefixText: "+91 - ",
+                                  labelStyle: TextStyle(color: Colors.pink),
+                                  icon: Icon(
+                                    Icons.mobile_friendly,
+                                    color: Colors.yellow,
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(
+                                        color: Colors.pink, width: 2),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(
+                                        color: Colors.pink, width: 2),
+                                  )),
                             ),
                           ),
                           visible: Visible,
@@ -114,14 +166,26 @@ class SignInUtil extends State<SignIn> {
                           child: Padding(
                             padding: EdgeInsets.all(15),
                             child: TextField(
+                              controller: address,
                               decoration: InputDecoration(
-                                labelText: "Address",
-                                icon: Icon(
-                                  Icons.location_city,
-                                  color: Colors.blueAccent,
-                                ),
-                                border: OutlineInputBorder(),
-                              ),
+                                  labelText: "Address",
+                                  hintText: "Input Address",
+                                  hintStyle: TextStyle(color: Colors.pinkAccent, fontStyle: FontStyle.normal),
+                                  labelStyle: TextStyle(color: Colors.pink),
+                                  icon: Icon(
+                                    Icons.location_city,
+                                    color: Colors.blueAccent,
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(
+                                        color: Colors.pink, width: 2),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(
+                                        color: Colors.pink, width: 2),
+                                  )),
                             ),
                           ),
                           visible: Visible,
@@ -133,24 +197,39 @@ class SignInUtil extends State<SignIn> {
                           crossAxisAlignment: CrossAxisAlignment.center,
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => MenuPage()));
-                              },
-                              child: Text(
-                                "🥗  ${buttonText}",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.normal,
-                                    fontStyle: FontStyle.normal),
+                            IgnorePointer(
+                              ignoring: ignore,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  if(buttonText == "SignUp"){
+                                    String userName = name.toString();
+                                    String userEmail = email.toString();
+                                    String userContact = contact.toString();
+                                    String userAddress = address.toString();
+                                    db?.onAddDetails(name: userName, email: userEmail, mobile_no: userContact, country: userAddress);
+
+                                  }
+                                  else if(buttonText == "LogIn"){
+                                    String userName = name.toString();
+                                    String userEmail = email.toString();
+                                  }
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => MenuPage()));
+                                },
+                                child: Text(
+                                  "🥗  ${buttonText}",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.normal,
+                                      fontStyle: FontStyle.normal),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    padding: EdgeInsets.all(10),
+                                    side: BorderSide(color: Colors.white),
+                                    shadowColor: Colors.purple,
+                                    elevation: 200),
                               ),
-                              style: ElevatedButton.styleFrom(
-                                  padding: EdgeInsets.all(10),
-                                  side: BorderSide(color: Colors.white),
-                                  shadowColor: Colors.purple,
-                                  elevation: 200),
                             )
                           ],
                         ),
@@ -167,6 +246,7 @@ class SignInUtil extends State<SignIn> {
                     onPressed: () {
                       setState(() {
                         Visible = false;
+                        ignore = false;
                         buttonText = "Login";
                       });
                     },
@@ -187,6 +267,7 @@ class SignInUtil extends State<SignIn> {
                     onPressed: () {
                       setState(() {
                         Visible = true;
+                        ignore = false;
                         buttonText = "SignUp";
                       });
                     },
